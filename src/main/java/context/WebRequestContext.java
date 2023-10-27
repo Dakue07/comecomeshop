62757ba0220a877d05e50969391346f02a1973ce
Dakue07 <yt22304014@ga.ttc.ac.jp>
2023-10-27 16:25:42 +0900

    a

---
+++ src/main/java/context/WebRequestContext.java
@@ -0,0 +1,45 @@
+package context;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+
+public class WebRequestContext implements RequestContext {
+	private Map<String, String[]> parameters;
+	private HttpServletRequest request;
+	private HttpSession session = null;
+	
+	public WebRequestContext() {}
+	
+	@Override
+	public String getCommandPath() {
+		String path = request.getRequestURI();
+		
+		String target = "";
+		return target;
+	}
+	
+	public String[] getParameter(String key) {
+		return parameters.get(key);
+	}
+	
+	public void setParameterMap(Map<String, String[]> map) {
+		parameters = map;
+	}
+	
+	public Object getRequest() {
+		return request;
+	}
+	
+	public void setRequest(Object request) {
+		this.request = (HttpServletRequest) request;
+	}
+	
+	public void InvalidateSession() {
+		session = request.getSession();
+		session.invalidate();
+	}
+}
---
+++ src/main/java/context/WebResponseContext.java
@@ -0,0 +1,35 @@
+package context;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class WebResponseContext implements ResponseContext {
+	private Object result;
+	private String target;
+	private HttpServletResponse response;
+	
+	public WebResponseContext() {}
+	
+	public Object getResult() {
+		return result;
+	}
+	
+	public String getTarget() {
+		return target;
+	}
+	
+	public void setResult(Object bean) {
+		result = bean;
+	}
+	
+	public void setTarget(String transferInfo) {
+		target = transferInfo;
+	}
+	
+	public void setResponse(Object obj) {
+		response = (HttpServletResponse)obj;
+	}
+	
+	public Object getResponse() {
+		return response;
+	}
+}
