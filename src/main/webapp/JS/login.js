37b8c1cfb1046fbb895bb54a419802db3395734d
nukko <sk22304008@ga.ttc.ac.jp>
2023-10-30 17:12:01 +0900

    消えたとことの修正

---
+++ src/main/webapp/JS/login.js
@@ -0,0 +1,17 @@
+﻿var showPasswordButton = document.getElementById("showPasswordButton");
+if (showPasswordButton) {
+  showPasswordButton.addEventListener("click", togglePasswordVisibility);
+}
+
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("passwordInput");
+  if (passwordInput) {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      showPasswordButton.textContent = "非表示";
+    } else {
+      passwordInput.type = "password";
+      showPasswordButton.textContent = "表示";
+    }
+  }
+}
---
+++ src/main/webapp/JSP/toppage.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" pageEncoding="UTF-8"
+    contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>toppage</title>
+</head>
+<body>
+    <!-- header -->
+    <%@include file="../assets/template/header.jsp" %>
+    
+    <!-- 中身 -->
+    
+    <!-- footer -->
+    <%@include file="../assets/template/footer.jsp" %>
+</body>
+</html>
