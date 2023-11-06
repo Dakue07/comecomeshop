e0c76718f2aabcbfed5c5cd25234e243170cd832
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2023-11-06 12:45:19 +0900

    追加。

---
+++ src/main/java/dto/UsertableDto.java
@@ -0,0 +1,36 @@
+package dto;
+
+public class UsertableDto {
+	private int userId; //これ間違えてるかも
+	private String user;
+	private String pass;
+	
+	public UsertableDto(String user, String pass) {
+		setUser(user);
+		setPass(pass);
+	}
+
+	
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+}
