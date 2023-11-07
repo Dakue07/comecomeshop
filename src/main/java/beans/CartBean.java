fe76bb91cca150aef3bb603ad392dcaecc4a65b7
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2023-11-07 11:40:19 +0900

    Bean追加

---
+++ src/main/java/beans/CartBean.java
@@ -0,0 +1,19 @@
+package beans;
+
+public class CartBean {
+	private int user_id;
+	private int rice_id;
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getRice_id() {
+		return rice_id;
+	}
+	public void setRice_id(int rice_id) {
+		this.rice_id = rice_id;
+	}
+}
