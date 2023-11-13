220373c18ab326d4a06e0256303a110f715e2a5d
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2023-11-13 17:03:30 +0900

    住所を入力したら町の名前まで入力してくれるようにしました。

---
+++ src/main/webapp/JS/postcodejp_script.js
@@ -0,0 +1,22 @@
+function initPostcodeJp() {
+
+    // APIキーを指定して住所補完サービスを作成
+    var postcodeJp = new postcodejp.address.AutoComplementService('ZsJmUiaZfpOP65MdUEoJ7NjjZnggXFHKa1LfsCL');//引数はAPIKey
+
+    // 郵便番号テキストボックスを指定
+    postcodeJp.setZipTextbox('input_zip')
+
+    // 住所補完フィールドを追加
+    postcodeJp.add(new postcodejp.address.StateTownTextbox('input_state_town'));
+    postcodeJp.add(new postcodejp.address.StreetTextbox('input_street'));
+    
+
+    // 郵便番号テキストボックスの監視を開始
+    postcodeJp.observe();
+
+    }
+    if(window.addEventListener){
+        window.addEventListener('load', initPostcodeJp)
+    }else{
+        window.attachEvent('onload', initPostcodeJp)
+    }
