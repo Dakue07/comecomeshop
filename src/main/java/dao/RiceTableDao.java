6ca0793c4ade6302664c5f8cfb6e1097231fe161
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2023-11-17 09:45:54 +0900

    product作成

--- src/main/java/dao/RiceTableDao.java
+++ src/main/java/dao/RiceTableDao.java
@@ -12,6 +12,7 @@ import dto.RiceTableDto;
 public class RiceTableDao {
     //private static final String SELECT_RICE_ALL = "SELECT * FROM RiceTable";
     private static final String SELECT_RICE_NAME = "SELECT * FROM RiceTable WHERE rice_name LIKE ? AND rice_flag = 0 ORDER BY rice_id DESC" ;
+    private static final String SELECT_PRODUCT = "SELECT * FROM RIceTable where rice_id = ?";
 //    private static final String DB_USER = "come";
 //    private static final String DB_PASS = "come";
         
@@ -23,48 +24,47 @@ public class RiceTableDao {
     ResultSet rs = null;
     
     
-//    public ArrayList<RiceTableDto> SelectAllRice() {
-//        ArrayList<RiceTableDto> result = new ArrayList<>();
-//        st = ma.getStatement(DB_USER, DB_PASS);
-//        ResultSet rs = null;
-//        try {
-//            rs = st.executeQuery(SELECT_RICE_ALL);
-//        
+    public ArrayList<RiceTableDto> SelectProduct(String rice_id) {
+        ArrayList<RiceTableDto> result = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+        	cn = ma.getConnection();
+            rs = st.executeQuery(SELECT_PRODUCT);
+        
 //            ResultSetMetaData rsMeta = (ResultSetMetaData) rs.getMetaData();
 //            int colCount = rsMeta.getColumnCount();
 //            
-//            while(rs.next()) {
-//                RiceTableDto ricedto= new RiceTableDto();
+            while(rs.next()) {
+                RiceTableDto ricedto= new RiceTableDto();
 //                for(int i = 1; i < colCount; i ++) {
-//                    ricedto.setRice_id(rs.getInt("rice_id"));
-//                    ricedto.setRice_name(rs.getString("rice_name"));
-//                    ricedto.setRice_genre(rs.getString("rice_genre"));
-//                    ricedto.setRice_weight(rs.getInt("rice_weight"));
-//                    ricedto.setRice_made(rs.getString("rice_made"));
-//                    ricedto.setRice_image(rs.getString("rice_imag"));
-//                    ricedto.setRice_since(rs.getString("rice_since"));
-//                    ricedto.setRice_stock(rs.getInt("rice_stock"));
-//                    ricedto.setRice_price(rs.getInt("rice_price"));
-//                    //result.add(ricedto);わかんねえ
-//
-//                }
-//                result.add(ricedto);
-//            }
-//        
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if(rs != null) {
-//                    rs.close();
-//                }
-//            } catch(SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return result;
-//        
-//    }
+                    ricedto.setRice_id(rs.getInt("rice_id"));
+                    ricedto.setRice_name(rs.getString("rice_name"));
+                    ricedto.setRice_genre(rs.getString("rice_genre"));
+                    ricedto.setRice_weight(rs.getInt("rice_weight"));
+                    ricedto.setRice_made(rs.getString("rice_made"));
+                    ricedto.setRice_image_path(rs.getString("rice_imag"));
+                    ricedto.setRice_since(rs.getString("rice_since"));
+                    ricedto.setRice_stock(rs.getInt("rice_stock"));
+                    ricedto.setRice_price(rs.getInt("rice_price"));
+                    result.add(ricedto);
+
+                }
+            //}
+        
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(rs != null) {
+                    rs.close();
+                }
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+        
+   }
 //    
     public ArrayList<RiceTableDto> SelectRice(String rice_name, String sortColmnName) { //使うのかわからない
     	ArrayList<RiceTableDto> result = new ArrayList<RiceTableDto>();
