87fda1c406c7de2e3d7c9a3c8eba9f5a97417f7e
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2023-11-17 11:20:39 +0900

    スペルミス

--- src/main/java/dao/UserAddressDao.java
+++ src/main/java/dao/UserAddressDao.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import database.MySQLOperator;
 
 public class UserAddressDao {
-	private static final String INSERT_ADDRESS = "INSERT INTO useraddresstable(user_id, useraddress_postcode, useraddress_state_sity, useraddress_street) values(?, ?, ?, ?)";
+	private static final String INSERT_ADDRESS = "INSERT INTO useraddresstable(user_id, useraddress_postcode, useraddress_state_city, useraddress_street) values(?, ?, ?, ?)";
 	
 	MySQLOperator ma = new MySQLOperator();
 	
@@ -16,12 +16,12 @@ public class UserAddressDao {
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 	
-	public boolean insertAddress(Connection cn, String user_id, String useraddress_postcode, String useraddress_state_sity, String useraddress_street) {
+	public boolean insertAddress(Connection cn, String user_id, String useraddress_postcode, String useraddress_state_city, String useraddress_street) {
 		try {
 			PreparedStatement pstmt = cn.prepareStatement(INSERT_ADDRESS);
 			pstmt.setString(1, user_id);
 			pstmt.setString(2, useraddress_postcode);
-			pstmt.setString(3, useraddress_state_sity);
+			pstmt.setString(3, useraddress_state_city);
 			pstmt.setString(4, useraddress_street);
 			int row = pstmt.executeUpdate();
 			return row > 0;
