3938a2d8b730d8e3b7b74301b13761fa24a5690e
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2023-11-20 16:38:09 +0900

    Cartだおとdto

--- src/main/java/dao/CartTableDao.java
+++ src/main/java/dao/CartTableDao.java
@@ -3,37 +3,62 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import database.MySQLOperator;
-import dto.CartTableDto;
+import dto.RiceCartTableDto;
 
 public class CartTableDao {
-	private static final String SELECT_ALL = "SELECT * FROM CartTable";
+	private static final String SELECT_ALL = "SELECT cart_quantity, rice_name, rice_genre, rice_weight, rice_made, rice_image_path, rice_since, rice_stock, rice_price "
+											+ "FROM CartTable INNER JOIN RiceTable ON CartTable.rice_id = RiceTable.rice_id WHERE CartTable.user_id = ?";//カートに追加したものをすべて表示する
+	private static final String INSERT_CART = "INSERT INTO CartTable VALUES(?, ?, ?)";//カートに追加するとき
 	
 	MySQLOperator ma = new MySQLOperator();
 	
 	Connection cn = null;
-	PreparedStatement prstm = null;
+	PreparedStatement prsmt = null;
 	ResultSet rs = null;
 	
-	public CartTableDto AllSelect() {
-		CartTableDto cdto = null;
+	public void CartInsert(int cart_quantity, int user_id, int rice_id) {//違うかも
 		try {
 			cn = ma.getConnection();
-			prstm = cn.prepareStatement(SELECT_ALL);
-			rs = prstm.executeQuery();
-			ResultSetMetaData rsMeta = rs.getMetaData();
-			int colCount = rsMeta.getColumnCount();
+			cn.setAutoCommit(false);
+			prsmt = cn.prepareStatement(INSERT_CART);
+			prsmt.setInt(1, cart_quantity);
+			prsmt.setInt(2, user_id);
+			prsmt.setInt(3, rice_id);
+			prsmt.executeUpdate();
+			cn.commit();
+		} catch(SQLException e) {
+			try {
+				cn.rollback();
+			} catch(SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	public  ArrayList<RiceCartTableDto> AllSelect(String user_id) {
+		ArrayList<RiceCartTableDto> result = new ArrayList<RiceCartTableDto>();
+		try {
+			cn = ma.getConnection();
+			prsmt = cn.prepareStatement(SELECT_ALL);
+			prsmt.setString(1, user_id);
+			rs = prsmt.executeQuery();
 			
 			while(rs.next()) {
-				cdto = new CartTableDto();
-				for(int i = 1; i < colCount; i++) {
-					cdto.setCart_quantity(rs.getInt("cart_quentity"));
-					cdto.setUser_id(rs.getInt("user_id"));
-					cdto.setRice_id(rs.getInt("rice_id"));
-				}
+				RiceCartTableDto rcdto = new RiceCartTableDto();
+				rcdto.setCart_quantity(rs.getInt("cart_quantity"));
+				rcdto.setRice_name(rs.getString("rice_name"));
+				rcdto.setRice_genre(rs.getString("rice_genre"));
+				rcdto.setRice_weight(rs.getString("rice_weight"));
+				rcdto.setRice_made(rs.getString("rice_made"));
+				rcdto.setRice_image_path(rs.getString("rice_image_path"));
+				rcdto.setRice_since(rs.getString("rice_since"));
+				rcdto.setRice_stock(rs.getString("rice_stock"));
+				rcdto.setRice_price(rs.getString("rice_price"));
+				result.add(rcdto);
 			}
 		} catch (SQLException e) {
             // TODO 自動生成された catch ブロック
@@ -47,6 +72,6 @@ public class CartTableDao {
                 e.printStackTrace();
             }
         }
-        return cdto;
+        return result;
 	}
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -18,7 +18,7 @@ public class UserDao {
 	MySQLOperator ma = new MySQLOperator();
 	
 	Connection cn = null;
-	PreparedStatement pstmt = null;
+	PreparedStatement prsmt = null;
 	ResultSet rs = null;
 	
 	public Connection connect() {
@@ -31,9 +31,9 @@ public class UserDao {
 		UserTableDto dto = null;
 		try {
 			cn = connect(); 
-			pstmt = cn.prepareStatement(SELECT_USER_PASS);
-			pstmt.setString(1, user);
-			rs = pstmt.executeQuery();
+			prsmt = cn.prepareStatement(SELECT_USER_PASS);
+			prsmt.setString(1, user);
+			rs = prsmt.executeQuery();
 			if(rs != null && rs.next()) {
 				dto = new UserTableDto(rs.getString(1), rs.getString(2));
 			} else {
@@ -49,11 +49,11 @@ public class UserDao {
 		try {
 			cn = connect();
 			cn.setAutoCommit(false);
-			PreparedStatement pstmt = cn.prepareStatement(INSERT_USER);
-			pstmt.setString(1, name);
-			pstmt.setString(2, pass);
-			pstmt.setString(3, mail);
-			int row = pstmt.executeUpdate();
+			prsmt = cn.prepareStatement(INSERT_USER);
+			prsmt.setString(1, name);
+			prsmt.setString(2, pass);
+			prsmt.setString(3, mail);
+			int row = prsmt.executeUpdate();
 			UserAddressDao uDao = new UserAddressDao();
 			uDao.insertAddress(cn, name, useraddress_postcord, useraddress_state_sity, useraddress_street);
 			cn.commit();
@@ -75,10 +75,10 @@ public class UserDao {
 		
 		try {
 			cn = connect();
-			pstmt = cn.prepareStatement(SELECT_USER_ID);
-			pstmt.setString(1, user_name);
-			pstmt.setString(2, user_pass);
-			rs = pstmt.executeQuery();
+			prsmt = cn.prepareStatement(SELECT_USER_ID);
+			prsmt.setString(1, user_name);
+			prsmt.setString(2, user_pass);
+			rs = prsmt.executeQuery();
 			rs.next();
 			user_id = rs.getString(1);
 			System.out.println(user_id);
