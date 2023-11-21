2b42c2a8e39cc169ed577fb10f16e5858cb96671
Dakue07 <yt22304014@ga.ttc.ac.jp>
2023-11-21 14:52:40 +0900

    なおし

--- src/main/java/commands/ProductsListCommand.java
+++ src/main/java/commands/ProductsListCommand.java
@@ -12,26 +12,12 @@ public class ProductsListCommand extends AbstractCommand {
 	public ResponseContext execute(ResponseContext resc) {
 		RequestContext reqc = getRequestContext();
 		Object result = null;
-		String sort = null;
-		String search = null;
 		
 		UserRiceBean userRiceBean = new UserRiceBean();
 		
 		RiceTableDao riceDao = new RiceTableDao();
 		
-		if(reqc.getParameter("search")[0] != null) {
-			search = reqc.getParameter("search")[0];
-		}
-		
-		if(reqc.getParameter("sort")[0] != null) {
-			sort = reqc.getParameter("sort")[0];
-		}
-		
-//		search = reqc.getParameter("search") != null ? reqc.getParameter("search")[0] : null;
-//		sort = reqc.getParameter("sort") != null ? reqc.getParameter("sort")[0] : null;
-//チャッピー
-		
-		result = riceDao.SelectRice(sort, search);
+		result = riceDao.SelectRice(null, null);
 		
 		userRiceBean.setRiceDto((ArrayList)result);
 		userRiceBean.setUserBean(((WebRequestContext) reqc).getUserBeanInSession());
