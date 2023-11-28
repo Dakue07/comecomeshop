128c6b55554512a01f3d88859210aa7fa3b28bd8
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2023-11-28 16:17:49 +0900

    userの削除とturefalseの変更

--- src/main/java/dao/RiceTableDao.java
+++ src/main/java/dao/RiceTableDao.java
@@ -13,8 +13,11 @@ public class RiceTableDao {
     //private static final String SELECT_RICE_ALL = "SELECT * FROM RiceTable";
     private static final String SELECT_RICE_NAME = "SELECT * FROM RiceTable WHERE rice_name LIKE ? AND rice_flag = 0 ORDER BY rice_id DESC" ;
     private static final String SELECT_PRODUCT = "SELECT * FROM RIceTable where rice_id = ?";
-//    private static final String DB_USER = "come";
-//    private static final String DB_PASS = "come";
+    private static final String INSERT_PRODUCT = "INSERT INTO RiceTable (rice_name, rice_genre, rice_weight, rice_made, rice_image_path, rice_since, rice_stock, rice_price, rice_flag) Values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String CHANGE_TRUE = "UPDATE RiceTable SET rice_flag = 1 WHERE rice_id = ?";
+    private static final String CHANGE_FALSE = "UPDATE RiceTable SET rice_flag = 0 WHERE rice_id = ?";
+
+
         
     Connection cn = MySQLOperator.getInstance().getConnection();
     Statement st = null;
@@ -105,5 +108,46 @@ public class RiceTableDao {
         }
         System.out.println(result.size());
         return result;
+    }
+    
+    public void insertProduct(String rice_name, String rice_genre, int rice_weight, String rice_made, String rice_image_path, String rice_since, int rice_stock, int rice_price, String rice_flag) {
+    	try {
+            cn = MySQLOperator.getInstance().getConnection();
+            prstm = cn.prepareStatement(INSERT_PRODUCT);
+            prstm.setString(1, rice_name);
+            prstm.setString(2, rice_genre);
+            prstm.setInt(3, rice_weight);
+            prstm.setString(4, rice_made);
+            prstm.setString(5, rice_image_path);
+            prstm.setString(6, rice_since);
+            prstm.setInt(7, rice_stock);
+            prstm.setInt(8, rice_price);
+            prstm.setString(9, rice_flag);
+            prstm.executeUpdate();
+    	} catch(SQLException e) {
+			MySQLOperator.getInstance().rollback();
+    	}
+    }
+    
+    public void changeTure(String rice_id) {
+    	try {
+            cn = MySQLOperator.getInstance().getConnection();
+            prstm = cn.prepareStatement(CHANGE_TRUE);
+            prstm.setString(1, rice_id);
+            prstm.executeUpdate();
+    	} catch(SQLException e) {
+			MySQLOperator.getInstance().rollback();
+    	}
+    }
+    
+    public void changeFalse(String rice_id) {
+    	try {
+            cn = MySQLOperator.getInstance().getConnection();
+            prstm = cn.prepareStatement(CHANGE_FALSE);
+            prstm.setString(1, rice_id);
+            prstm.executeUpdate();
+    	} catch(SQLException e) {
+			MySQLOperator.getInstance().rollback();
+    	}
     }
 }
