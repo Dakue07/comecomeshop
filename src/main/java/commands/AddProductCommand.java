f7679f61ad366dae25cb3f3b5b6ca8bd2f1e92f6
Dakue07 <yt22304014@ga.ttc.ac.jp>
2023-11-29 16:33:57 +0900

    いえーーーーーい

---
+++ src/main/java/commands/AddProductCommand.java
@@ -0,0 +1,35 @@
+package commands;
+
+import context.RequestContext;
+import context.ResponseContext;
+import dao.RiceTableDao;
+import database.MySQLOperator;
+
+public class AddProductCommand extends AbstractCommand {
+	public ResponseContext execute(ResponseContext resc) {
+		
+		RequestContext reqc = getRequestContext();
+		
+		MySQLOperator.getInstance().beginTransaction();
+		
+		String rice_name = reqc.getParameter("rice_name")[0];
+		String rice_genre = reqc.getParameter("rice_genre")[0];
+		int rice_weight = Integer.valueOf(reqc.getParameter("rice_weight")[0]);
+		String rice_made = reqc.getParameter("rice_made")[0];
+		String rice_image_path = reqc.getParameter("rice_image_path")[0];
+		String rice_since = reqc.getParameter("rice_since")[0];
+		int rice_stock = Integer.valueOf(reqc.getParameter("rice_stock")[0]);
+		int rice_price = Integer.valueOf(reqc.getParameter("rice_price")[0]);
+		
+		RiceTableDao riceDao = new RiceTableDao();
+
+		riceDao.insertProduct(rice_name,rice_genre, rice_weight, rice_made, rice_image_path, rice_since, rice_stock, rice_price);
+		
+		MySQLOperator.getInstance().commit();
+		
+		resc.setTarget("productmanage");
+		
+		return resc;
+	}
+
+}
