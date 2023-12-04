e5845e8a6995b1708c18b3ac26b1ec1083c3ac17
Yung <hy22304024@ga.ttc.ac.jp>
2023-12-04 11:56:07 +0900

    AWS用

--- src/main/java/controller/ApplicationController.java
+++ src/main/java/controller/ApplicationController.java
@@ -1,10 +1,12 @@
 package controller;
 
+import java.io.InputStream;
+
 import context.RequestContext;
 import context.ResponseContext;
 
 public interface ApplicationController {
 	RequestContext getRequest(Object request);
-	ResponseContext handleRequest(RequestContext req, String rootPath);
+	ResponseContext handleRequest(RequestContext req, InputStream file);
 	void handleResponse(RequestContext req, ResponseContext res);
 }
