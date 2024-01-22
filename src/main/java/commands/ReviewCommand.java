38a0bb850c51a7e979346e5e26d8ab205558cc45
kazu20010926 <ki22304002@ga.ttc.ac.jp>
2024-01-22 10:44:11 +0900

    引数変更

--- src/main/java/commands/ReviewCommand.java
+++ src/main/java/commands/ReviewCommand.java
@@ -1,6 +1,5 @@
 package commands;
 
-import beans.UserBean;
 import context.RequestContext;
 import context.ResponseContext;
 import dao.ReviewTableDao;
@@ -10,13 +9,11 @@ public class ReviewCommand extends AbstractCommand {
 		RequestContext reqc = getRequestContext();
 		Object result = null;
 		
-		UserBean userBean = reqc.getUserBeanInSession();
-		
-		int user_id = userBean.getUser_id();
+		String rice_id = reqc.getParameter("rice_id")[0];
 		
 		ReviewTableDao reviewdao = new ReviewTableDao();
 		
-		result = reviewdao.Select_Review(user_id);
+		result = reviewdao.Select_Review(rice_id);
 		
 		resc.setResult(result);
 		
