06613346245bd89d8e7e0610655e4df5d36a9962
Dakue07 <yt22304014@ga.ttc.ac.jp>
2024-01-30 18:42:19 +0900

    むりぽ②

--- src/main/java/commands/OrderCancelCommand.java
+++ src/main/java/commands/OrderCancelCommand.java
@@ -1,5 +1,8 @@
 package commands;
 
+import java.util.ArrayList;
+
+import beans.OrderedBean;
 import context.RequestContext;
 import context.ResponseContext;
 import dao.OrderTableDao;
@@ -12,19 +15,27 @@ public class OrderCancelCommand extends AbstractCommand {
 		
 		MySQLOperator.getInstance().beginTransaction();
 		
-		int user_id = reqc.getUserBeanInSession().getUser_id();
-		int order_id = Integer.parseInt(reqc.getParameter("order_id")[0]);
-		int rice_id = Integer.parseInt(reqc.getParameter("rice_id")[0]);
-		int order_quantity = Integer.parseInt(reqc.getParameter("order_quantity")[0]);
-		
-		System.out.println("米キャンセル" + rice_id);
-		System.out.println("米キャンセル" + order_quantity);
-		
 		OrderTableDao orderdao = new OrderTableDao();
 		RiceTableDao ricedao = new RiceTableDao();
 		
+		
+		int rice_id;
+		int order_quantity;
+		int user_id = reqc.getUserBeanInSession().getUser_id();
+		int order_id = Integer.parseInt(reqc.getParameter("order_id")[0]);
+		
+		ArrayList<OrderedBean> orderlist = new ArrayList<OrderedBean>();
+		
+		orderlist = orderdao.selectOrderByOrderId(order_id);
+		
+		for (int i = 0; i < orderlist.size(); i++) {
+			rice_id = orderlist.get(i).getRice_id();
+			order_quantity = orderlist.get(i).getOrder_quantity();
+			ricedao.orderCancel(rice_id, order_quantity);
+		}
+		
 		orderdao.orderCancel(order_id);
-		ricedao.orderCancel(rice_id, order_quantity);
+		
 		
 		MySQLOperator.getInstance().commit();
 		
